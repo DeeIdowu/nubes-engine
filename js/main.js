0e69c834f655c8da0731c8f4cf657a75dad49fd4
Dennis Idowu <deethegreat@Denniss-MacBook-Air.local>
2019-05-07 20:35:02 -0400

    edits

---
+++ js/main.js
@@ -0,0 +1,37 @@
+$(document).ready(function() {
+  //wow.js on scroll animations initialization
+  wow = new WOW({
+    animateClass: "animated",
+    mobile: false,
+    offset: 50
+  });
+  wow.init();
+
+  //parallax effect initialization
+  $(".hero").parallax("50%", 0.3);
+
+  //Nice scroll initialization
+  $("html").niceScroll({
+    scrollspeed: 50,
+    autohidemode: false,
+    cursorwidth: 8,
+    cursorborderradius: 8,
+    cursorborder: "0",
+    background: "rgba(48, 48, 48, .4)",
+    cursorcolor: "#1f1f1f",
+    zindex: 999
+  });
+
+  //Testimonials slider initialization
+  $("#tslider").owlCarousel({
+    items: 1,
+    navigation: true,
+    pagination: false,
+    slideSpeed: 300,
+    paginationSpeed: 400,
+    singleItem: true,
+    responsive: true,
+    autoPlay: true,
+    transitionStyle: "fade"
+  });
+});
